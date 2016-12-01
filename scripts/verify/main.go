e5a7d9e80400123e61e90b07562ea54549392cc9
Manish R Jain <manish@dgraph.io>
2016-12-01 16:15:59 +1100

    Update the golden data, and perfect the verify script.

--- scripts/verify/main.go
+++ scripts/verify/main.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"bufio"
+	"bytes"
 	"compress/gzip"
 	"flag"
 	"fmt"
+	"io/ioutil"
 	"os"
 	"sort"
 
@@ -16,6 +18,7 @@ import (
 var (
 	src  = flag.String("src", "", "Gzipped RDF data file.")
 	dst  = flag.String("dst", "", "Gzipped RDF data file.")
+	out  = flag.String("out", "diff.rdf", "Output differences to this file.")
 	head = flag.Int("head", -1, "Number of lines to read from head. -1 means read all.")
 	glog = logrus.WithField("pkg", "verify")
 )
@@ -25,6 +28,13 @@ type R struct {
 	p string
 	o uint64
 	v string
+
+	os string
+	oo string
+}
+
+func (r R) String() string {
+	return fmt.Sprintf("%x %s %x %s [%s,%s]", r.s, r.p, r.o, r.v, r.os, r.oo)
 }
 
 type ByR []R
@@ -33,17 +43,27 @@ func (b ByR) Len() int {
 	return len(b)
 }
 
+func equal(a, b R) bool {
+	if a.s != b.s ||
+		a.p != b.p ||
+		a.o != b.o ||
+		a.v != b.v {
+		return false
+	}
+	return true
+}
+
 func less(bi, bj R) bool {
-	if bi.s < bj.s {
-		return true
+	if bi.s != bj.s {
+		return bi.s < bj.s
 	}
-	if bi.p < bj.p {
-		return true
+	if bi.p != bj.p {
+		return bi.p < bj.p
 	}
-	if bi.o < bj.o {
-		return true
+	if bi.o != bj.o {
+		return bi.o < bj.o
 	}
-	return bi.v <= bj.v
+	return bi.v < bj.v
 }
 
 func (b ByR) Less(i int, j int) bool {
@@ -73,6 +93,7 @@ func getScanner(fname string) (*os.File, *bufio.Scanner) {
 func convert(n rdf.NQuad) R {
 	r := R{}
 	var err error
+	r.os = n.Subject
 	r.s, err = rdf.GetUid(n.Subject)
 	x.Checkf(err, "Subject: %v", n.Subject)
 	r.p = n.Predicate
@@ -80,28 +101,38 @@ func convert(n rdf.NQuad) R {
 	if len(n.ObjectId) > 0 {
 		r.o, err = rdf.GetUid(n.ObjectId)
 		x.Checkf(err, "Object: %v", n.ObjectId)
+		r.oo = n.ObjectId
 	}
 	r.v = string(n.ObjectValue)
 	return r
 }
 
 func compare(srcl, dstl []R) {
-	for _, s := range srcl {
-		i := sort.Search(len(dstl), func(i int) bool {
-			return less(s, dstl[i])
-		})
-		if i < len(dstl) {
-			if s == dstl[i] {
-				fmt.Printf("MATCH: %v %v\n", s, dstl[i])
-			} else {
-				fmt.Printf("DIFFERENT: %v %v\n", s, dstl[i])
-				return
-			}
+	var buf bytes.Buffer
+	var ps, ds, loop, matches int
+	for ps < len(srcl) && ds < len(dstl) {
+		loop++
+		if loop%100 == 0 {
+			//fmt.Printf(".")
+		}
+		s := srcl[ps]
+		d := dstl[ds]
+		if equal(s, d) {
+			//fmt.Printf("Found match: %v\n", s)
+			matches++
+			ps++
+			ds++
+		} else if less(s, d) {
+			// s < d, advance s
+			buf.WriteString(s.String())
+			buf.WriteRune('\n')
+			ps++
 		} else {
-			fmt.Printf("NO MATCH: %x %v %x %q\n", s.s, s.p, s.o, s.v)
-			return
+			ds++
 		}
 	}
+	fmt.Printf("Found matches: %v\n", matches)
+	x.Check(ioutil.WriteFile(*out, buf.Bytes(), 0644))
 }
 
 func main() {
@@ -137,5 +168,8 @@ func main() {
 	sort.Sort(ByR(srcl))
 	sort.Sort(ByR(dstl))
 	fmt.Println("Comparing now")
+	//for i := 0; i < 100; i++ {
+	//fmt.Printf("[S,D] %v %v\n", srcl[i], dstl[i])
+	//}
 	compare(srcl, dstl)
 }
