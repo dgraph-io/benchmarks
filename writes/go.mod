d724cc92c2960549240ba8903aedf4c4aa5ef3d4
Karl McGuire <karl@xba.io>
2021-02-12 12:19:51 -0500

    batched reader

--- writes/go.mod
+++ writes/go.mod
@@ -3,7 +3,7 @@ module github.com/dgraph-io/benchmarks/writes
 go 1.15
 
 require (
-	github.com/davecgh/go-spew v1.1.1 // indirect
-	github.com/minio/minio-go/v6 v6.0.57 // indirect
-	github.com/minio/minio-go/v7 v7.0.8 // indirect
+	github.com/davecgh/go-spew v1.1.1
+	github.com/dustin/go-humanize v1.0.0
+	github.com/minio/minio-go/v7 v7.0.8
 )
--- writes/writes.go
+++ writes/writes.go
@@ -1,15 +1,57 @@
 package main
 
 import (
-	"bytes"
 	"context"
+	"crypto/rand"
 	"flag"
-	"io/ioutil"
+	"fmt"
+	"io"
 
 	"github.com/davecgh/go-spew/spew"
+	"github.com/dustin/go-humanize"
 	minio "github.com/minio/minio-go/v7"
 	"github.com/minio/minio-go/v7/pkg/credentials"
 )
+
+type ReaderSmall struct {
+	i int
+}
+
+func (r *ReaderSmall) Read(p []byte) (n int, err error) {
+	size := 117000000 // 117 MB (standard backup batch size)
+	if size > len(p) {
+		size = len(p)
+	}
+	fmt.Printf("minio asks for %s, providing %s\n",
+		humanize.Bytes(uint64(len(p))),
+		humanize.Bytes(uint64(size)))
+	r.i++
+	if r.i == 12 {
+		fmt.Println("EOF")
+		err = io.EOF
+		return
+	}
+	n, err = rand.Read(p[:size])
+	return
+}
+
+type ReaderFull struct {
+	i int
+}
+
+func (r *ReaderFull) Read(p []byte) (n int, err error) {
+	fmt.Printf("minio asks for %s, providing %s\n",
+		humanize.Bytes(uint64(len(p))),
+		humanize.Bytes(uint64(len(p))))
+	r.i++
+	if r.i == 2 {
+		fmt.Println("EOF")
+		err = io.EOF
+		return
+	}
+	n, err = rand.Read(p)
+	return
+}
 
 var (
 	AccessKey = flag.String("access-key", "", "AWS Access Key")
@@ -20,14 +62,17 @@ func init() {
 	flag.Parse()
 }
 
+func upload(c *minio.Client, r io.Reader) (minio.UploadInfo, error) {
+	return c.PutObject(
+		context.Background(),
+		"dgraph-backup-testing",
+		"throughput-test",
+		r,
+		-1,
+		minio.PutObjectOptions{})
+}
+
 func main() {
-	data, err := ioutil.ReadFile("/home/karl/random") // 1gb file
-	if err != nil {
-		panic(err)
-	}
-
-	dataReader := bytes.NewReader(data)
-
 	c, err := minio.New("s3.amazonaws.com", &minio.Options{
 		Creds:  credentials.NewStaticV4(*AccessKey, *SecretKey, ""),
 		Secure: true,
@@ -36,14 +81,13 @@ func main() {
 		panic(err)
 	}
 
-	// starts uploading immediately
-	spew.Dump(
-		c.PutObject(
-			context.Background(),
-			"dgraph-backup-testing",
-			"throughput-test",
-			dataReader,
-			int64(len(data)),
-			minio.PutObjectOptions{}),
-	)
+	fmt.Println("full reader:")
+	// starts uploading immediately, automatically does s3 multipart
+	spew.Dump(upload(c, &ReaderFull{}))
+
+	fmt.Println("\nbatched reader:")
+	// only starts uploading once the full p []byte is filled by Reader
+	//
+	// if p []byte is never fully filled, it only starts uploading once EOF is reached
+	spew.Dump(upload(c, &ReaderSmall{}))
 }
